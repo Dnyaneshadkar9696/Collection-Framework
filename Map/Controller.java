285e458302de2f2821ee33200431bc8971163696
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-03-20 22:47:58 +0530

    HashMap

---
+++ Map/Controller.java
@@ -0,0 +1,14 @@
+package collectionframework;
+
+public class Controller {
+	
+	public static void main(String[] args) {
+		
+//		Questions sd = new Questions();
+//		sd.call();
+		
+		Maps m = new Maps();
+		m.callMap();
+	}
+
+}
